30750a8ed0b9c63991896d03d6f96bc13c7d9daa
Utkarsh Mehta <utkarsh@tarkalabs.com>
2021-11-24 20:49:42 +0530

    hands on

---
+++ HandsOn/10_Switches.js
@@ -0,0 +1,39 @@
+/**
+ * Use Switch Case to implement the following
+ * 1. Write a switch case which does the following
+ *      a. print addition of (10, 20) when operator is add
+ *      b. print subtraction of (50, 20) when operator is minus
+ *      c. print multiplication of (15, 2) when operator is multiply
+ *      d. print division of (60, 2) when operator is divide
+ */
+
+/**
+ * 
+ * @param {*} operator can be one of the following ['add', 'minus', 'multiply', 'divide']
+ */
+function getValue(operator) {
+
+}
+
+/**
+ * Use Switch Case to calculate salary
+ * salary = (perHourWage * 168) * (1 - (taxRate/100))
+ *  
+ * |╔══════════════════╦═══════════════╦══════════╗|
+ * |║     Position     ║ Per Hour Wage ║ Tax Rate ║|
+ * |╠══════════════════╬═══════════════╬══════════╣|
+ * |║ Support          ║ $5            ║ 3%       ║|
+ * |║ Developer        ║ $20           ║ 8%       ║|
+ * |║ Senior Developer ║ $35           ║ 14%      ║|
+ * |║ Tech Lead        ║ $60           ║ 25%      ║|
+ * |║ Director         ║ $100          ║ 40%      ║|
+ * |╚══════════════════╩═══════════════╩══════════╝|
+ */
+
+/**
+ * 
+ * @param {*} operator can be one of the following ['add', 'minus', 'multiply', 'divide']
+ */
+ function getSalary(position) {
+
+}
---
+++ HandsOn/11_Loops.js
@@ -0,0 +1,51 @@
+/**
+ * For Loop
+ * Write a for loop to print following pattern
+ * 
+ *                  #####
+ *                  ####
+ *                  ###
+ *                  ##
+ *                  #
+ */
+
+
+/**
+ * For In Loop
+ * Count the strings from following array.  
+ */
+let arr = ['1', 12, false, "world", {}, [1, 23, 3]];
+
+
+/**
+ * While Loop
+ * Use while loop to find x
+ * where ((x * 123) + 321) / 321 > 12345678
+ */
+
+
+/**
+ * Do While Loop
+ * Convert a decimal number to binary
+ * Example: 7
+ * |╔═══════════╦══════════╦═══════════╗|
+ * |║ Operation ║ Division ║ Remainder ║|
+ * |╠═══════════╬══════════╬═══════════╣|
+ * |║ 8 / 2     ║        4 ║         0 ║|
+ * |║ 4 / 2     ║        2 ║         0 ║|
+ * |║ 2 / 2     ║        1 ║         0 ║|
+ * |║ 1 / 2     ║        0 ║         1 ║|
+ * |╚═══════════╩══════════╩═══════════╝|
+ * 
+ * Ans = 1000
+ * 
+ * |╔═══════════╦══════════╦═══════════╗|
+ * |║ Operation ║ Division ║ Remainder ║|
+ * |╠═══════════╬══════════╬═══════════╣|
+ * |║ 7 / 2     ║        3 ║         1 ║|
+ * |║ 3 / 2     ║        1 ║         1 ║|
+ * |║ 1 / 2     ║        0 ║         1 ║|
+ * |╚═══════════╩══════════╩═══════════╝|
+ * 
+ * Ans = 111
+ */
