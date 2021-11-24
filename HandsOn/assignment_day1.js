411350dfd48964fad9ebd594200b6d9fe2fceaff
Utkarsh Mehta <utkarsh@tarkalabs.com>
2021-11-24 17:09:51 +0530

    added assignment

---
+++ HandsOn/assignment_day1.js
@@ -0,0 +1,16 @@
+var name = "Utkarsh";
+
+// Use name variable to save Uk as nickname 
+// (you can use slice/substring/substr & concat)
+var nickname;
+console.log("nickname =>", nickname);
+
+var age = "27";
+// convert age to number
+// console log number of months in 27 years
+
+const agenda = ["Movie", "Games", "Work", "Food", "Shopping"];
+
+// Print Games by assigning correct index
+let index;
+console.log(agenda[index])
