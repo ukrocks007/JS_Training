fedcf87968fb40d472994c162a4f06db45ca7164
Utkarsh Mehta <utkarsh@tarkalabs.com>
2021-11-25 13:51:17 +0530

    changes

--- 8_Operators.js
+++ 8_Operators.js
@@ -32,6 +32,8 @@ console.log("(x++) =>", (x));
 x--;
 console.log("(x--) =>", (x));
 
+// Operation and assignment ( a OP= b => a = a OP b)
+// a+=b => a = a + b
 x = 4, y = 3;
 x += y;
 console.log("(x += y) =>", x)
@@ -51,7 +53,7 @@ x = 2, y = 3;
 x **= y;
 console.log("(x **= y) =>", x)
 
-// ==	equal to
+// ==	equal to (value)
 // ===	equal value and equal type
 // !=	not equal
 // !==	not equal value or not equal type
@@ -71,6 +73,8 @@ console.log("x >= y =>", x >= y);
 console.log("x <= y =>", x <= y);
 console.log("x > y ? y : x =>", x > y ? y : x);
 
-console.log("x > y && y < 20", x > y && y < 20);
-console.log("x > y || y < 20", x > y || y < 20);
-console.log("!(x > 10)", !(x > 10));
+console.log("x > y && y < 20", x > y && y < 20); // Both the expressions should be true
+console.log("x > y || y < 20", x > y || y < 20); // Any one of the expression should be true
+console.log("!(x > 10)", !(x > 10)); // Flips the output (T=>F, F=>T)
+
+// <condition> ? <return when true> : (<condition2> ? <return when true> : <return when false>)
